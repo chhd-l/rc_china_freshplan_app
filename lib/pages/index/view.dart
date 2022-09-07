cb42cf65eb124e9cfc4a95548b780eccd3addc8c
zuoqin <qzuo@deloitte.com.cn>
2022-09-07 11:13:27 +0800

    assets 资源放置

--- lib/pages/index/view.dart
+++ lib/pages/index/view.dart
@@ -5,10 +5,14 @@ class IndexPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('welcome to rc_china_freshplan_app'),
-      ),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 251, 179, 6),
+      body: Column(children: [
+        const SizedBox(height: 100,),
+        Image.asset('assets/images/homeward.png',
+            width: 51, fit: BoxFit.fitWidth),
+        const Text('welcome to rc_china_freshplan_app',style: TextStyle(fontSize: 20),)
+      ]),
     );
   }
 }
