10650a930313b62246cc30bea28dc928e11e1666
chhd <xscnwan13@163.com>
2022-09-14 15:26:54 +0800

    订单列表

--- lib/common/router/app_pages.dart
+++ lib/common/router/app_pages.dart
@@ -18,7 +18,7 @@ import 'package:rc_china_freshplan_app/common/util/storage.dart';
 class AppPages {
   static String initial = StorageUtil().getJSON('loginUser') == null
       ? AppRoutes.login
-      : AppRoutes.addressManage;
+      : AppRoutes.orderList;
 
   static final routes = [
     GetPage(
