c7d5428f3c3fe74b0e8bff48fd5a56019bae2431
chhd <xscnwan13@163.com>
2022-09-14 09:55:36 +0800

    地址

--- lib/common/router/app_pages.dart
+++ lib/common/router/app_pages.dart
@@ -17,7 +17,7 @@ import 'package:rc_china_freshplan_app/common/util/storage.dart';
 class AppPages {
   static String initial = StorageUtil().getJSON('loginUser') == null
       ? AppRoutes.login
-      : AppRoutes.addressManage;
+      : AppRoutes.index;
 
   static final routes = [
     GetPage(
