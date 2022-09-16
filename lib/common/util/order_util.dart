c24144566da315e22aa7d8c49c2abd9524c71968
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2022-09-16 18:18:28 +0800

    order list null

--- lib/common/util/order_util.dart
+++ lib/common/util/order_util.dart
@@ -2,20 +2,20 @@ import 'package:rc_china_freshplan_app/api/order/index.dart';
 import 'package:rc_china_freshplan_app/data/order.dart';
 
 class OrderUtil {
-  static List orderLists= [];
+  static List orderLists = [];
 
   static Future getOrders(String text) async {
     print('getOrders');
     print(text);
     var params = {};
-    if(text != 'ALL') {
+    if (text != 'ALL') {
       params['orderState'] = text;
     }
     var data = await OrderEndPoint.getOrders(10, 0, params);
     if (data != false) {
-      orderLists = data['records'];
+      orderLists = data['records'] ?? [];
     }
-    return data['records'];
+    return data['records'] ?? [];
   }
 
   static Future getOrderDetail(String orderNum) async {
